214419f9d37e83b3f313b4e5cdccf8c7d98f9675
Ville Puolitaival <ville.puolitaival@gmail.com>
2023-02-21 00:28:06 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Bomb from './things/Bomb';
+
+const App = () => {
+  return (
+    <div>
+      <Bomb />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/things/Bomb/index.tsx
@@ -0,0 +1,5 @@
+const Bomb = () => {
+  return <div>This would be a Bomb if it was implemented</div>;
+};
+
+export default Bomb;
